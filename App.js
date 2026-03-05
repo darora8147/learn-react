66a9eab749ce354f8922d48c45a3c49d93958d1c
darora8147 <divyamarora95@gmail.com>
2026-03-05 18:04:42 +0530

    Initial Commit

---
+++ App.js
@@ -0,0 +1,16 @@
+//Nested HTML Structure using React.createElement
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "I am h1 tag in child 1"),
+    React.createElement("h2", {}, "I am h2 tag in child 1"),
+  ]),
+);
+
+// const heading = React.createElement("h1", {id: "heading"}, "Hello World from React!!");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
